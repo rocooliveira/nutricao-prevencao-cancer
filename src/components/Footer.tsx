684d23f49ffcf3891f0164761f79746700bf690a
rocooliveira <rocooliveira@hotmail.com>
2022-05-04 11:49:35 -0300

    1 file modified

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { Stack, Flex, Box, Image, Text } from '@chakra-ui/react';
+
+export function Footer() {
+  return (
+    <Flex bg="gray.00">
+      <Stack>
+        <Box p={6}></Box>
+      </Stack>
+    </Flex>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import {
 import { Header } from '../components/Header';
 import { CheckItem } from '../components/CheckItem';
 import { IconCard } from '../components/IconCard';
+import { Footer } from '../components/Footer';
 
 import styles from '../styles/common.module.css';
 
@@ -284,6 +285,8 @@ export default function Home() {
             </Box>
           </Stack>
         </Stack>
+
+        <Footer />
       </main>
     </div>
   );

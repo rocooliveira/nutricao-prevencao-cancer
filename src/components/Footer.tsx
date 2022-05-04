f2923cbbd0001b4725c9e670c872f2adf8f7a19d
rocooliveira <rocooliveira@hotmail.com>
2022-05-04 12:43:14 -0300

    2 files modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,24 @@
-import { Stack, Flex, Box, Image, Text } from '@chakra-ui/react';
+import { Stack, Flex, Box, Image, Text, Fo } from '@chakra-ui/react';
+import styles from '../styles/common.module.css';
 
 export function Footer() {
   return (
-    <Flex bg="gray.700">
-      <Stack>
-        <Box p={6}></Box>
-      </Stack>
-    </Flex>
+    <footer>
+      <Flex>
+        <Stack>
+          <Box className={styles.footerOverlayMainWaves} />
+          <Box className={styles.footerOverlayBlackWaves} />
+        </Stack>
+        <Stack bg="gray.800" position="relative" w="100%" top={10}>
+          <Box p={6} mt={12}>
+            <Text as="b" color="white">
+              SEMINARIO MULTIDISCIPLINAR
+            </Text>
+            <Text>fsfs</Text>
+            <Text>fsfs</Text>
+          </Box>
+        </Stack>
+      </Flex>
+    </footer>
   );
 }

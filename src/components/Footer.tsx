d84e670a113a1a083e2890b79b9912a7b0d1f20c
rocooliveira <rocooliveira@hotmail.com>
2022-05-04 11:56:58 -0300

    1 file modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Stack, Flex, Box, Image, Text } from '@chakra-ui/react';
 
 export function Footer() {
   return (
-    <Flex bg="gray.00">
+    <Flex bg="gray.700">
       <Stack>
         <Box p={6}></Box>
       </Stack>

ccc962ab6b06e1afa27bca89365b065e4eb7b93b
Romulo Costa <Roco@MacBook-Pro-de-Edgar.local>
2022-05-05 16:02:58 -0300

    files added

---
+++ src/components/RecipesCard.tsx
@@ -0,0 +1,34 @@
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
+
+interface RecipesCardProps{
+  title:    string;
+  subTitle: string;
+  picture:  string;
+}
+export function RecipesCard({title, subTitle, picture}: RecipesCardProps){
+    return(
+        <Box
+        p={6}
+        py={[6, 12]}
+        my={[20, 52]}
+        w="100%"
+        bg="white"
+        borderRadius="xl"
+        shadow="md"
+        borderWidth="1px"
+        >
+          <Stack>
+              <Box mt={['-6rem', '-14rem']}>
+                  <Image src={picture} w={['70%', '60%']} mx="auto"/>
+              </Box>
+              <Box>
+                <Text fontSize={['xl', '2xl']} fontWeight="bold">{title}</Text>
+                <Text mt={4} fontSize={['md', '2xl']}>{subTitle}</Text>
+              </Box>
+              <Box>
+                <Text as="u" fontSize="xs" color="green.400" fontWeight="bold">Ver receita</Text>
+              </Box>
+          </Stack>
+        </Box>
+    );
+}
